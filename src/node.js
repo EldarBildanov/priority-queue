6fcf7e9f92d16d89fb102ea23068614e2e254ade
EldarBildanov <eldar.bildanov@gmail.com>
2019-10-16 15:43:46 +0300

    feat: add task not full

--- src/node.js
+++ src/node.js
@@ -1,22 +1,47 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data
+		this.priority = priority
+		this.parent = null
+		this.left = null
+		this.right = null
+		
 	}
 
 	appendChild(node) {
+		if (!node) return this;
 
+		if (!this.left) {
+		  node.parent = this;
+		  this.left = node;
+		} else if (!this.right) {
+		  node.parent = this;
+		  this.right = node;
+		}
 	}
 
 	removeChild(node) {
-
+		if (!node) return this
+		if (this.left != node) 
+			if (this.right != node) 
+			throw new Error("Not a child")
+		if (this.left) this.left = null
+		if (this.right) this.right = null
+		node.parent = null	
 	}
 
 	remove() {
-
+		if (!this.parent) return this
+		this.parent.removeChild(this)
 	}
 
 	swapWithParent() {
-		
+		if (!this.parent) return this
+		let parent = this.parent
+		let leftChild = this.left
+		let rightChild = this.right
+		let grandParent = parent.parent
+
 	}
 }
 
--- src/queue.js
+++ src/queue.js
@@ -2,7 +2,9 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		this.max = maxSize
+		if (!maxSize) this.max = 30
+		this.heap = new MaxHeap()
 	}
 
 	push(data, priority) {
